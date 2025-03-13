abbfb75decbce70edb4a732d6b5b40aa23a23893
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-03-13 13:02:13 +0300

    added OOP version

--- compare_with_saved_data.py
+++ compare_with_saved_data.py
@@ -11,3 +11,19 @@ def compare_with_saved_data(base_dir, int_images_number):
             text_file.truncate()
             return True
         return False
+
+class CompareWithSavedData:
+    def __init__(self, base_dir, text_file_name, any_date):
+        self.base_dir = base_dir
+        self.any_date = any_date
+        self.text_file_name = text_file_name
+
+    def compare(self):
+        with open(self.text_file_name, 'r+') as text_file:
+            if text_file.read().strip() != str(self.any_date):
+                text_file.seek(0)
+                text_file.write(str(int(self.any_date)))
+                text_file.truncate()
+                return True
+            return False
+
